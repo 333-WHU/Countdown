73ea687df929e06474a38a949b6b9ae785db1687
333-WHU <147116549+333-WHU@users.noreply.github.com>
2024-10-23 21:19:37 +0800

    Create Button_Box.py
    所有按钮样式的类

---
+++ Button_Box.py
@@ -0,0 +1,115 @@
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget, QPushButton, QSpinBox,QHBoxLayout,QApplication
+from PySide6.QtCore import QTimer, QTime, Qt,QDate
+from PySide6.QtGui import QIcon, QFont,QPixmap
+
+class BaseButton(QPushButton):
+    """基础按钮"""
+    def __init__(self):
+        super().__init__()
+        self.init_size()
+        self.init_self()
+        self.init_background()
+
+    def init_size(self):
+        """初始化大小"""
+        self.setMinimumHeight(90)
+        self.setMaximumWidth(150)
+    
+    def init_self(self):
+        """初始化部件"""
+
+    def init_background(self):
+        """初始化样式"""
+        self.setStyleSheet("""
+                color: red;
+                border-radius: 10px;
+            """)
+
+class UpButton(BaseButton):
+    """增加时间按钮"""
+    def __init__(self):
+        super().__init__()
+
+    def init_size(self):
+        """初始化大小"""
+        self.setMinimumHeight(90)
+        self.setMaximumWidth(150)
+    
+    def init_self(self):
+        """初始化部件"""
+        self.printer = QPixmap("./sourse/UpArrow_del.png")
+
+
+    def init_background(self):
+        
+        self.layout = QHBoxLayout()
+        self.label = QLabel()
+        self.layout.addWidget(self.label)
+        self.setLayout(self.layout)
+        self.label.setPixmap(self.printer)
+        self.setStyleSheet("""
+                color: red;
+                border-radius: 10px;
+            """)
+        
+class DownButton(UpButton):
+    """增加时间按钮"""
+    def __init__(self):
+        super().__init__()
+
+    def init_self(self):
+        """初始化部件"""
+        self.printer = QPixmap("./sourse/DownArrow_del.png")
+
+class ConfirmButton(BaseButton):
+    """确定按钮"""
+    def __init__(self):
+        super().__init__()
+
+    def init_size(self):
+        self.setMaximumSize(100, 50)
+        self.setMinimumSize(100, 50)
+
+    def init_self(self):
+        self.setText("确定")
+        
+
+    def init_background(self):
+        self.setStyleSheet("""
+                color: #5ba585;
+                background-color: #e1e1de;
+                border-radius: 10px;
+                """)
+
+class FinishButton(BaseButton):
+    """完成按钮"""
+    def __init__(self):
+        super().__init__()
+
+    def init_size(self):
+        self.setMaximumSize(100, 50)
+        self.setMinimumSize(100, 50)
+
+    def init_self(self):
+        self.setText("提前结束")
+
+    def init_background(self):
+        self.setStyleSheet("""
+                color: #5ba585;
+                background-color: #e1e1de;
+                border-radius: 10px;
+                            """)
+        
+
+
+if __name__ == '__main__':
+    app  = QApplication([])
+    window = QWidget()
+    window.setWindowTitle("Button")
+
+    layout = QVBoxLayout()  
+    layout.addWidget(UpButton())
+    layout.addWidget(DownButton())
+    window.setLayout(layout)
+    window.show()
+    app.exec()
